c2fe0aff3325c24d4e30715d6e28bb46ab5f9f06
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-02-13 00:20:01 -0500

    Basic annotations - concat

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
+"""
+Define a function named 'concat' that concatenates two strings.
+"""
+
+
 def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+
+    Args:
+        str1 (str): The first string to concatenate.
+        str2 (str): The second string to concatenate.
+
+    Returns:
+        str: The string resulting from the concatenation of str1 and str2.
+    """
     return str1 + str2
