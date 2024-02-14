6c9205d34a04597a2c55314fbf967cd447065480
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-02-13 23:47:35 -0500

    Define variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""We define four variables with type annotations."""
+
+"""The variable 'a' is an integer with value 1."""
+a: int = 1
+"""The variable 'pi' is a float with value 3.14."""
+pi: float = 3.14
+"""The variable 'i_understand_annotations' is a boolean with value True."""
+i_understand_annotations: bool = True
+"""The 'school' variable is a character string with the value "Holberton"."""
+school: str = "Holberton"
