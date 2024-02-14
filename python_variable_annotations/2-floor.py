dea3f23f9711ef14e6c147208c3fefb576cb5ad0
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-02-13 23:17:48 -0500

    Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import math
+"""
+This function takes a float as input and
+returns its closest minor integer value.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the input float.
+
+    Parameters:
+        n (float): The input float.
+
+    Returns:
+        int: The floor of the input float.
+    """
+    return math.floor(n)
