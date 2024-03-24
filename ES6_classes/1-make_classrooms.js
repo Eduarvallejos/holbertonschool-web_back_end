1b962c82fecf2599547ee63e4c6e83d6bf1a29f6
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-03-24 11:42:25 -0500

    A function is implemented that returns 3 objects

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -2,12 +2,12 @@
 import ClassRoom from './0-classroom';
 
 // Implement the initializeRooms function
-export default function initializeRooms () {
+export default function initializeRooms() {
   // Create an array of ClassRoom objects with sizes 19, 20, and 34
   const rooms = [
     new ClassRoom(19),
     new ClassRoom(20),
-    new ClassRoom(34)
+    new ClassRoom(34),
   ];
 
   return rooms;// Return the array of ClassRoom objects
