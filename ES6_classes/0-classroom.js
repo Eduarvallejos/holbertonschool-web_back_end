d0c6a3ff11f68074972251e9c6ba2a530db71b71
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-03-24 10:45:42 -0500

    defining class

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,5 +1,8 @@
+// Definition of the ClassRoom class
 export default class ClassRoom {
+    // Constructor of the ClassRoom class
+    // It is initialized with an optional maximum size for the students
     constructor(maxStudentsSize = 0) {
-        this._maxStudentsSize = maxStudentsSize
+        this._maxStudentsSize = maxStudentsSize; // Initialization of the maximum student size
     }
 }
