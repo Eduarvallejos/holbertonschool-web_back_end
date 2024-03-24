b8b61a6d4693e28679a7f2d0dcfaa911b2419a28
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-03-24 18:47:17 -0500

    Airport

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,14 +1,12 @@
-// Airport class definition
-export default class Airport {
-  constructor(name = '', code = '') {
-    // Initialize name and code attributes
-    this._name = name;
-    this._code = code;
-  }
+// Class Airport
 
-  // toString method definition
-  toStringTag() {
-    // Returns a string representation of the Airport objec
-    return `[objeto ${this._code}]`;
+export default class Airport {
+    constructor(name = '', code = '') {
+      this._name = name;
+      this._code = code;
+    }
+  
+    toString() {
+      return `[object ${this._code}]`;
+    }
   }
-}
