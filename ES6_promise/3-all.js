56be4e6864dc6bf12996b797c17e877a5390230e
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-04-14 13:11:01 -0500

    Handle multiple successful promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+// Import functions from utils.js
+import { uploadPhoto, createUser } from './utils';
+
+// Define the handleProfileSignup function
+export default function handleProfileSignup() {
+  // Promise.all is used to wait for the resolution of both promises
+  Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      // Destructure the results to access the body of each response
+      const [photoResult, userResult] = results;
+      console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
+    })
+    // Handle the error in case one of the promises is rejected
+    .catch(() => console.log('Signup system offline'));
+}
