061dfa84b4d909c6ad97b435a9a4d580338b25f6
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-04-14 12:24:35 -0500

    new promise

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,4 +1,4 @@
 // Keep every promise you make and only make promises you can keep
-export default function getResponseFromAPI () {
+export default function getResponseFromAPI() {
   return new Promise((resolve) => resolve());
 }
