b1489705d7a57ef5bf8c1d5eecf6b40c717ac427
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-04-14 14:09:47 -0500

    Throw error / try catch

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,19 @@
+export default function guardrail (mathFunction) {
+  // Create an array to store the values and messages.
+  const queue = [];
+
+  try {
+    // Execute the mathFunction function and add its return value to the array
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    // If the function throws an error, add the error message to the array
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    // Add the processing message to the array,
+    // regardless of whether the function was executed or an error was caught
+    queue.push('Guardrail was processed');
+  }
+  // Return the resulting array
+  return queue;
+}
