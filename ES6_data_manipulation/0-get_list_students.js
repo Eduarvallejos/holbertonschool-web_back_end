4460a37a01b7622f02a211c1a885d180e58d8c66
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-04-19 23:47:16 -0500

    Basic list of objects

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,9 @@
+// This script returns a list of objects
+
 export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Fransisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 3, firstName: 'Serena', location: 'San Fransisco' }
+    { id: 5, firstName: 'Serena', location: 'San Fransisco' },
   ];
 }
