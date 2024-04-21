93864ef67cf6faab837bf780366903831158d3ee
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-04-21 18:34:25 -0500

    More map data structure

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,14 @@
 // returns an updated map for all items with an initial quantity of 1.
 
-export default function updateUniqueItems(map) {
+export default function updateUniqueItems (map) {
   // Check if the argument is a Map
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
   // Iterate over each entry in the map and update the quantity if it is 1
   map.forEach((quantity, item) => {
-    quantity === 1 && map.set(item, 100);
+    if (quantity === 1) {
+      map.set(item, 100);
+    }
   });
 }
