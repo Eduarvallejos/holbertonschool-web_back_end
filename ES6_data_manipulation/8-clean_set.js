f4ce193ccfc4a9a6d3d0aa6e45a010ca63b3a798
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-04-21 17:48:05 -0500

    Clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,18 +1,17 @@
 // This script returns a string of all set values ​​starting with a specific string (startString)
 
 export default function cleanSet(set, startString) {
-  // Initialize an empty array to store filtered elements
-  const filtervalues = [];
-  // Iterate over each value in the set
-  for (const valor of set) {
-    // Check if the value starts with the specified startString
-    if (valor.startsWith(startString)) {
-      // Append the rest of the string to the filteredValues array
-      filtervalues.push(valor.slice(startString.length));
-    }
-  }
-  // Join the filtered values with '-' separator to form the result string
-  const result = filtervalues.join('-');
+    // Convertir el conjunto (Set) en un array utilizando el operador de propagación (...)
+    const arrayFromSet = [...set];
+    
+    // Filtrar los elementos del array que empiezan con la cadena especificada
+    const filteredValues = arrayFromSet.filter(element =>
+        element.startsWith(startString)
+    );
+    
+    // Unir los elementos filtrados en un string separados por '-'
+    const result = filteredValues.join('-');
 
-  return result;
+    return result;
 }
+
