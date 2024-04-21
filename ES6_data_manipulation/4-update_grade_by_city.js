f7401c989f88ee48fb1c6bcb30bc177ced3e114a
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-04-21 15:14:46 -0500

    Combine

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,17 +2,17 @@
 
 export default function updateStudentGradeByCity(liststudents, city, newGrades) {
   // Filter students who are in the specific city
-  const studentsInCity = liststudents.filter(student => student.location === city);
+  const studentsInCity = liststudents.filter((student) => student.location === city);
   // Map about students in the city and update their grades
-  const updateStudents = studentsInCity.map(student => {
+  const updateStudents = studentsInCity.map((student) => {
     // Check if there is a new grade for the student in newGrades
-    const newGrade = newGrades.find(grade => grade.studentId === student.id);
+    const newGrade = newGrades.find((grade) => grade.studentId === student.id);
     // If a new rating is found, update it; otherwise set it to N/A
     return {
       id: student.id,
       firstName: student.firstName,
       location: student.location,
-      grade: newGrade ? newGrade.grade : 'N/A'
+      grade: newGrade ? newGrade.grade : 'N/A',
     };
   });
   return updateStudents;
